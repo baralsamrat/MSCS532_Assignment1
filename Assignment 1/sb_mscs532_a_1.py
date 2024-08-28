68662ca6e27effd4c32bf30ffbc8a1bb2ddf2551
Samrat Baral <sbaral30064@ucumberlands.edu>
2024-08-28 17:23:23 -0500

    Update sb_mscs532_a_1.py
    Added test, comment the algorithm used.

--- Assignment 1/sb_mscs532_a_1.py
+++ Assignment 1/sb_mscs532_a_1.py
@@ -1,5 +1,16 @@
-#  Insertion Sort Algorithm that sorts in monotonically decreasing order
+# Insertion Sort Algorithm that sorts in monotonically decreasing order
+
+
 def insertion_sort_algo(NUM):
+    """
+    This function implements the insertion sort algorithm to sort a list of numbers in monotonically decreasing order.
+
+    Parameters:
+    NUM (list): The list of numbers to be sorted
+
+    Returns:
+    None. The function sorts the list in-place.
+    """
     for i in range(1, len(NUM)):
         key = NUM[i]
         j = i - 1
@@ -8,7 +19,14 @@ def insertion_sort_algo(NUM):
             j -= 1
         NUM[j + 1] = key
 
-# Example usage
+# Test function
+my_list = [5, 1, 2, 1, 4]
+print("Original list:", my_list)
+insertion_sort_algo(my_list)
+print("Sorted list:", my_list)
+
+
+# Get the list of numbers from the user
 my_list = input("Enter a list of numbers to sort: ").split()
 my_list = [int(num) for num in my_list]
 
